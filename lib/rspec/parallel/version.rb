ffcc11a15ed65abc1f72df90fb0bf7b393937a6c
Ram Kumar K R <ramkumar-kr@users.noreply.github.com>
2016-06-19 17:36:21 +0530

    Update version.rb

--- lib/rspec/parallel/version.rb
+++ lib/rspec/parallel/version.rb
@@ -3,7 +3,7 @@ module RSpec
     # Version information for RSpec Parallel.
     module Version
       # Current version of RSpec Parallel, in semantic versioning format.
-      STRING = '>= 3.1.0'
+      STRING = '3.1.0'
     end
   end
 end
